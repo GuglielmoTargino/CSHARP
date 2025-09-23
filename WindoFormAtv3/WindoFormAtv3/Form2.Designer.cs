7276f7d168df55eec3bed2231fc820b9bbe0ebc9
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-09-23 01:24:29 -0300

    atv3
    atualização do exercício 3.

---
+++ WindoFormAtv3/WindoFormAtv3/Form2.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace WindoFormAtv3
+{
+    partial class TelaDoc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.doc = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnTela3 = new System.Windows.Forms.Button();
+            this.textBoxRG = new System.Windows.Forms.TextBox();
+            this.textBoxCPF = new System.Windows.Forms.TextBox();
+            this.textBoxCNH = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButtonMas = new System.Windows.Forms.RadioButton();
+            this.radioButtonFem = new System.Windows.Forms.RadioButton();
+            this.btnTela2 = new System.Windows.Forms.Button();
+            this.btnTela4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // doc
+            // 
+            this.doc.AutoSize = true;
+            this.doc.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.doc.Location = new System.Drawing.Point(316, 49);
+            this.doc.Name = "doc";
+            this.doc.Size = new System.Drawing.Size(186, 29);
+            this.doc.TabIndex = 0;
+            this.doc.Text = "Documentação";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(99, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "RG";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(102, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CPF";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(102, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CNH";
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(155, 383);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 4;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnTela3
+            // 
+            this.btnTela3.Location = new System.Drawing.Point(380, 383);
+            this.btnTela3.Name = "btnTela3";
+            this.btnTela3.Size = new System.Drawing.Size(75, 23);
+            this.btnTela3.TabIndex = 5;
+            this.btnTela3.Text = "Tela 3";
+            this.btnTela3.UseVisualStyleBackColor = true;
+            // 
+            // textBoxRG
+            // 
+            this.textBoxRG.Location = new System.Drawing.Point(252, 134);
+            this.textBoxRG.Name = "textBoxRG";
+            this.textBoxRG.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRG.TabIndex = 6;
+            // 
+            // textBoxCPF
+            // 
+            this.textBoxCPF.Location = new System.Drawing.Point(252, 185);
+            this.textBoxCPF.Name = "textBoxCPF";
+            this.textBoxCPF.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCPF.TabIndex = 7;
+            // 
+            // textBoxCNH
+            // 
+            this.textBoxCNH.Location = new System.Drawing.Point(252, 238);
+            this.textBoxCNH.Name = "textBoxCNH";
+            this.textBoxCNH.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCNH.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(105, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Sexo";
+            // 
+            // radioButtonMas
+            // 
+            this.radioButtonMas.AutoSize = true;
+            this.radioButtonMas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonMas.Location = new System.Drawing.Point(339, 288);
+            this.radioButtonMas.Name = "radioButtonMas";
+            this.radioButtonMas.Size = new System.Drawing.Size(89, 21);
+            this.radioButtonMas.TabIndex = 11;
+            this.radioButtonMas.TabStop = true;
+            this.radioButtonMas.Text = "Masculino";
+            this.radioButtonMas.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonFem
+            // 
+            this.radioButtonFem.AutoSize = true;
+            this.radioButtonFem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonFem.Location = new System.Drawing.Point(487, 288);
+            this.radioButtonFem.Name = "radioButtonFem";
+            this.radioButtonFem.Size = new System.Drawing.Size(83, 21);
+            this.radioButtonFem.TabIndex = 12;
+            this.radioButtonFem.TabStop = true;
+            this.radioButtonFem.Text = "Feminino";
+            this.radioButtonFem.UseVisualStyleBackColor = true;
+            // 
+            // btnTela2
+            // 
+            this.btnTela2.Location = new System.Drawing.Point(263, 382);
+            this.btnTela2.Name = "btnTela2";
+            this.btnTela2.Size = new System.Drawing.Size(75, 23);
+            this.btnTela2.TabIndex = 13;
+            this.btnTela2.Text = "Tela 2";
+            this.btnTela2.UseVisualStyleBackColor = true;
+            // 
+            // btnTela4
+            // 
+            this.btnTela4.Location = new System.Drawing.Point(494, 382);
+            this.btnTela4.Name = "btnTela4";
+            this.btnTela4.Size = new System.Drawing.Size(75, 23);
+            this.btnTela4.TabIndex = 14;
+            this.btnTela4.Text = "Tela 4";
+            this.btnTela4.UseVisualStyleBackColor = true;
+            // 
+            // TelaDoc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTela4);
+            this.Controls.Add(this.btnTela2);
+            this.Controls.Add(this.radioButtonFem);
+            this.Controls.Add(this.radioButtonMas);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxCNH);
+            this.Controls.Add(this.textBoxCPF);
+            this.Controls.Add(this.textBoxRG);
+            this.Controls.Add(this.btnTela3);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.doc);
+            this.Name = "TelaDoc";
+            this.Text = "TelaDoc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label doc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnTela3;
+        private System.Windows.Forms.TextBox textBoxRG;
+        private System.Windows.Forms.TextBox textBoxCPF;
+        private System.Windows.Forms.TextBox textBoxCNH;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButtonMas;
+        private System.Windows.Forms.RadioButton radioButtonFem;
+        private System.Windows.Forms.Button btnTela2;
+        private System.Windows.Forms.Button btnTela4;
+    }
+}
